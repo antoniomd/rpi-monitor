27cbd3a00ba48a243107e9f0814e69b8b9421e6c
Antonio Molina Dominguez <antoniomolinadominguez@MacBook-Air-de-Antonio.local>
2020-09-16 18:09:02 +0200

    Update

--- rpi-monitor.py
+++ rpi-monitor.py
@@ -1,15 +1,34 @@
-# python script
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+"""
+bullsAndCowsBot.py
+
+The script gets CPU and GPU temperatura and CPU uses.
+
+Then these parameters can be view in the Terminal.
+
+Usage:
+Clone the repository.
+
+Run rpi-monitor.py in your RPi.
+"""
+
+# Importo las librerias
 import commands, psutil
 
-# Obtiene la Tª de la CPU y de la GPU
-temp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3
-tempgpu = commands.getoutput('/opt/vc/bin/vcgencmd measure_temp' ).replace('temp=', '' ).replace(''C', '')
+def main():
+    # Obtiene la Tª de la CPU y de la GPU
+    temp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3
+    tempgpu = commands.getoutput('/opt/vc/bin/vcgencmd measure_temp' ).replace('temp=', '' ).replace(''C', '')
 
-# Obtiene el uso de la CPU
-cpusan = psutil.cpu_percent(interval=1, percpu=False)
+    # Obtiene el uso de la CPU
+    cpusan = psutil.cpu_percent(interval=1, percpu=False)
 
-# Muestra en el Terminal la temperatura de la CPU, la teperatura de la GPU y el porcentaje de uso de la CPU
-print 'Temp.CPU =>' + str(temp)
-print 'Temp.GPU =>' + str(tempgpu)
-print cpusan
+    # Muestra en el Terminal la temperatura de la CPU, la teperatura de la GPU y el porcentaje de uso de la CPU
+    print 'Temp.CPU =>' + str(temp)
+    print 'Temp.GPU =>' + str(tempgpu)
+    print cpusan
+
+if __name__ == "__main__":
+	main()
