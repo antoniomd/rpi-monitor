66aea604d25115fc3f57cffe89a94e4ca2ca71e4
Antonio Molina Dominguez <antoniomolinadominguez@MacBook-Air-de-Antonio.local>
2020-09-20 18:45:18 +0200

    Update

--- rpi-monitor.py
+++ rpi-monitor.py
@@ -1,10 +1,9 @@
-#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """
 bullsAndCowsBot.py
 
-The script gets CPU and GPU temperatura and CPU uses.
+The script gets CPU temperature and use.
 
 Then these parameters can be view in the Terminal.
 
@@ -15,20 +14,18 @@ Run rpi-monitor.py in your RPi.
 """
 
 # Importo las librerias
-import commands, psutil
+import subprocess#, psutil
 
 def main():
     # Obtiene la Tª de la CPU y de la GPU
     temp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3
-    tempgpu = commands.getoutput('/opt/vc/bin/vcgencmd measure_temp' ).replace('temp=', '' ).replace(''C', '')
 
     # Obtiene el uso de la CPU
-    cpusan = psutil.cpu_percent(interval=1, percpu=False)
+    # cpuUse = psutil.cpu_percent(interval=1, percpu=False)
 
     # Muestra en el Terminal la temperatura de la CPU, la teperatura de la GPU y el porcentaje de uso de la CPU
-    print 'Temp.CPU =>' + str(temp)
-    print 'Temp.GPU =>' + str(tempgpu)
-    print cpusan
+    print('Temp.CPU =>' + str(temp))
+    # print(cpuUse)
 
 if __name__ == "__main__":
 	main()
