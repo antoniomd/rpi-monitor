bb7833b975b50e408143b47417f1c08b3d3ac233
Antonio <64436906+antoniomd@users.noreply.github.com>
2020-09-21 16:19:52 +0200

    Update rpi-monitor.py

--- rpi-monitor.py
+++ rpi-monitor.py
@@ -3,29 +3,46 @@
 """
 rpi-monitor.py
 
-The script gets CPU temperature and use.
+The script gets CPU temperature, use and frequency.
 
-Then these parameters can be view in the Terminal.
+Then these parameters can be viewed in the Terminal.
 
 Usage:
 Clone the repository.
 
-Run rpi-monitor.py in your RPi.
+Run rpi-monitor.py in your RPi. Maybe, you must install psutil.
 """
 
 # Importo las librerias
-import subprocess#, psutil
+import subprocess, psutil
 
 def main():
-    # Obtiene la Tª de la CPU y de la GPU
-    temp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3
+    # Obtengo la Tª de la CPU
+    tempFile = open('/sys/class/thermal/thermal_zone0/temp', 'r')
+    cpuTemp = float(tempFile.read())/1000
+    tempFile.close()
 
-    # Obtiene el uso de la CPU
-    # cpuUse = psutil.cpu_percent(interval=1, percpu=False)
+    # Obtengo el % de uso de la CPU
+    cpuUse = psutil.cpu_percent(interval=1, percpu=False)
+    
+    # Obtengo la frecuencia de la CPU
+    freqFile = open('/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq', 'r')
+    cpuFreq = float(freqFile.read())/1000
+    freqFile.close()
+    
+    # Muestra en el Terminal la información
+    print('Temperatura CPU => ' + str(int(cpuTemp)) + ' ºC')
+    print('Uso de la CPU => ' + str(cpuUse) + ' %')
+    print('Frecuencia CPU => ' + str(cpuFreq) + ' Hz')
 
-    # Muestra en el Terminal la temperatura de la CPU, la teperatura de la GPU y el porcentaje de uso de la CPU
-    print('Temperatura CPU => ' + str(temp) + ' ºC')
-    # print(cpuUse)
+    # Gestión alarmas de temperatura
+    warningTemp = tripTemp = False
+    if cpuTemp > 80:
+        warningTemp = True
+    elif cpuTemp > 85:
+        tripTemp = True
+
+
 
 if __name__ == "__main__":
 	main()
