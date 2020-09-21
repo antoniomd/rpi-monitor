0c420ac7bd3a2cf8eb40bbf4cf6db153ca105988
Antonio <64436906+antoniomd@users.noreply.github.com>
2020-09-21 18:21:26 +0200

    Update rpi-monitor.sh

--- rpi-monitor.sh
+++ rpi-monitor.sh
@@ -5,5 +5,5 @@ cpuFreq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
 echo "$(date) @ $(hostname)"
 echo "-------------------------------------------"
 echo "Temperatura de la CPU => $((cpuTemp/1000)) ºC"
-echo "Frecuencia de la CPU => $((cpuFreq/1000)) Hz"
+echo "Frecuencia de la CPU => $((cpuFreq/1000)) MHz"
 echo "-------------------------------------------"
